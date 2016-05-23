097fd13af33360c26747b980bfb3ebd094a580a5
Sebastian Stein <sebastian.stein@in2code.de>
2016-05-23 14:52:48 +0200

    [FEATURE][WIP] entferne jquery.chosen, füge select2 hinzu

--- Resources/Private/JavaScript/In2studyfinder.js
+++ Resources/Private/JavaScript/In2studyfinder.js
@@ -36,16 +36,57 @@
 					filterHandling.filterChanged(paginationPage);
 				}
 			}
+
+			$('.select2').select2({
+					matcher: function(params, data) {
+						var status = false;
+						var term = params.term;
+						var text = data.text;
+
+						if ($.trim(term) === '') {
+							status = true;
+						}
+
+						if (data.id !== '' && $.trim(term) !== '') {
+							var element = data.element;
+							var keywords = $(element).attr('alt');
+							var keywordArray = keywords.split(',');
+
+							$.each(keywordArray, function(index, keyword) {
+								keyword = keyword.trim();
+								if (
+									text.toUpperCase().indexOf(term.toUpperCase()) > -1 ||
+									keyword.toUpperCase().indexOf(term.toUpperCase()) > -1
+								) {
+									status = true
+								}
+							});
+						}
+
+						if (status) {
+							return data;
+						} else {
+							return null;
+						}
+
+					},
+					placeholder: "Select an Studycourse",
+					allowClear: true
+				}
+			)
+			;
+
 		};
 	}
 
-	// export to global scope
+// export to global scope
 	if (!window.In2studyfinder) {
 		window.In2studyfinder = {};
 	}
 
 	window.In2studyfinder.Start = Start;
-})();
+})
+();
 
 jQuery(document).ready(function() {
 	var start = new window.In2studyfinder.Start();
