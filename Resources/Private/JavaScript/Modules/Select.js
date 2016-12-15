b71c5f3038c9812eb41295e0c41abd76d2ae050a
Sebastian Stein <sebastian.stein@in2code.de>
2016-12-15 17:16:43 +0100

    [TASK] add select viewhelper

--- Resources/Private/JavaScript/Modules/Select.js
+++ Resources/Private/JavaScript/Modules/Select.js
@@ -14,7 +14,7 @@
 
 						if (data.id !== '' && $.trim(term) !== '') {
 							var element = data.element;
-							var keywords = $(element).attr('value');
+							var keywords = $(element).attr('alt');
 							var keywordArray = keywords.split(',');
 
 							$.each(keywordArray, function(index, keyword) {
