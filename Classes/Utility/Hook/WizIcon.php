a4c31f43c6a99bf6f7fc5d91465d598cdca8ab23
Sebastian Stein <sebastian.stein@in2code.de>
2017-01-16 20:23:17 +0100

    [TASK] add In2studyfinder to ContentElementWizard for Typo3 6.2

---
+++ Classes/Utility/Hook/WizIcon.php
@@ -0,0 +1,64 @@
+<?php
+namespace In2code\In2studyfinder\Utility\Hook;
+
+use \TYPO3\CMS\Core\Utility\ExtensionManagementUtility;
+
+/***************************************************************
+ *
+ *  Copyright notice
+ *
+ *  (c) 2017 Sebastian Stein <sebastian.stein@in2code.de>, In2code GmbH
+ *
+ *  All rights reserved
+ *
+ *  This script is part of the TYPO3 project. The TYPO3 project is
+ *  free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  The GNU General Public License can be found at
+ *  http://www.gnu.org/copyleft/gpl.html.
+ *
+ *  This script is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  This copyright notice MUST APPEAR in all copies of the script!
+ ***************************************************************/
+
+/**
+ * Wizard Icon for In2studyfinder
+ */
+class WizIcon
+{
+
+    /**
+     * Path to locallang file (with : as postfix)
+     *
+     * @var string
+     */
+    protected $locallangPath = 'LLL:EXT:in2studyfinder/Resources/Private/Language/locallang_db.xlf:';
+
+    /**
+     * Processing the wizard items array
+     *
+     * @param array $wizardItems
+     * @return array
+     */
+    public function proc($wizardItems = array())
+    {
+        $wizardItems['plugins_tx_in2studyfinder_pi1'] = array(
+            'icon' => ExtensionManagementUtility::extRelPath('in2studyfinder') . 'ext_icon.png',
+            'title' => $GLOBALS['LANG']->sL($this->locallangPath . 'wizardItemTitle', true),
+            'description' => $GLOBALS['LANG']->sL($this->locallangPath . 'wizardItemDescription', true),
+            'params' => '&defVals[tt_content][CType]=list&defVals[tt_content][list_type]=in2studyfinder_pi1',
+            'tt_content_defValues' => array(
+                'CType' => 'list',
+            ),
+        );
+
+        return $wizardItems;
+    }
+}
