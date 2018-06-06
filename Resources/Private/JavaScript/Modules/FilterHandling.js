c14f7488abc5adb63671a02f9243c7619b8974c1
Sebastian Stein <sebastian.stein@in2code.de>
2018-06-06 12:14:06 +0200

    [BUGFIX] reduce the page type num from 2308171055 to 1308171055. The previous page type exceeded the maximum integer number in 32 bit systems

--- Resources/Private/JavaScript/Modules/FilterHandling.js
+++ Resources/Private/JavaScript/Modules/FilterHandling.js
@@ -39,7 +39,7 @@
 				sysLanguageUidQuery = '&L=' + sysLanguageUid;
 			}
 
-			var url = '/?type=2308171055&studyFinderAjaxRequest=1' + contentElementUidQuery + sysLanguageUidQuery;
+			var url = '/?type=1308171055&studyFinderAjaxRequest=1' + contentElementUidQuery + sysLanguageUidQuery;
 			if (paginationPage) {
 				url += '&tx_in2studyfinder_pi1%5B%40widget_0%5D%5BcurrentPage%5D=' + paginationPage;
 			}
