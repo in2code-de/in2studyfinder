a9009d1ec20b9ab98dc42f3a43e7bc12d09784ad
Sebastian Stein <sebastian.stein@in2code.de>
2019-03-13 10:34:41 +0100

    [TASK] set correct supported typo3 versions

--- ext_emconf.php
+++ ext_emconf.php
@@ -14,7 +14,7 @@ $EM_CONF['in2studyfinder'] = array(
     'version' => '6.0.0',
     'constraints' => array(
         'depends' => array(
-            'typo3' => '8.7.99-9.5.99',
+            'typo3' => '8.7.0-9.5.99',
         ),
         'conflicts' => array(),
         'suggests' => array(),
