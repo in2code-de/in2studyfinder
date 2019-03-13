cabdfd30193ecf521f8ee0db40a63cf7cb3ce014
Sebastian Stein <sebastian.stein@in2code.de>
2019-03-13 14:15:54 +0100

    [BUGFIX] add missing use

--- Classes/ViewHelpers/Form/SelectViewHelper.php
+++ Classes/ViewHelpers/Form/SelectViewHelper.php
@@ -3,6 +3,7 @@
 namespace In2code\In2studyfinder\ViewHelpers\Form;
 
 use In2code\In2studyfinder\Utility\ExtensionUtility;
+use TYPO3\CMS\Core\Utility\GeneralUtility;
 use TYPO3\CMS\Extbase\DomainObject\AbstractDomainObject;
 use TYPO3\CMS\Extbase\Mvc\Web\Routing\UriBuilder;
 use TYPO3\CMS\Extbase\Object\ObjectManager;
