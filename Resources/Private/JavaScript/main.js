2d2e071c04f3262ee8976b2ea8a52fec97070fc5
Sebastian Stein <sebastian.stein@in2code.de>
2019-03-28 21:52:00 +0100

    [BUGFIX] fix jquery requireJs path for typo3 9.7

--- Resources/Private/JavaScript/main.js
+++ Resources/Private/JavaScript/main.js
@@ -1,18 +1,18 @@
 requirejs.config({
-	baseUrl: requireJsBasePath,
-	paths: {
-		'jquery': '/typo3/sysext/core/Resources/Public/JavaScript/Contrib/jquery/jquery-3.2.1.min',
-		'TYPO3/CMS/In2studyfinder': '/typo3conf/ext/in2studyfinder/Resources/Public/JavaScript'
-	},
-	'shim': {
-		'select2': {
-			deps: ['jquery'],
-			exports: '$.select2'
-		}
-	}
+  baseUrl: requireJsBasePath,
+  paths: {
+    'jquery': jqueryBasePath,
+    'TYPO3/CMS/In2studyfinder': '/typo3conf/ext/in2studyfinder/Resources/Public/JavaScript'
+  },
+  'shim': {
+    'select2': {
+      deps: ['jquery'],
+      exports: '$.select2'
+    }
+  }
 });
 
 define(['TYPO3/CMS/In2studyfinder/Frontend'], function(Frontend) {
-	'use strict';
-	Frontend.initialize();
+  'use strict';
+  Frontend.initialize();
 });
