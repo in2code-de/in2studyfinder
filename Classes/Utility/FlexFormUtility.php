72c5f9e5db0326e527ce8da2c826ce76f0033ea8
Sebastian Stein <sebastian.stein@in2code.de>
2020-01-15 16:27:39 +0100

    [TASK] use the FlexFormUtility to get an instance of the flexFormService. This keeps the compatibility up to version TYPO3 8.7
    See: https://docs.typo3.org/c/typo3/cms-core/master/en-us/Changelog/9.4/Deprecation-85802-MoveFlexFormServiceFromEXTextbaseToEXTcore.html

---
+++ Classes/Utility/FlexFormUtility.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace In2code\In2studyfinder\Utility;
+
+class FlexFormUtility
+{
+    /**
+     * @return \TYPO3\CMS\Core\Service\FlexFormService|\TYPO3\CMS\Extbase\Service\FlexFormService
+     */
+    public static function getFlexFormService()
+    {
+        // TYPO3 V9 and above
+        if (class_exists(\TYPO3\CMS\Core\Service\FlexFormService)) {
+            $flexFormService = self::getObjectManager()->get(\TYPO3\CMS\Core\Service\FlexFormService::class);
+        } else {
+            // TYPO3 V8 and below
+            $flexFormService = self::getObjectManager()->get(\TYPO3\CMS\Extbase\Service\FlexFormService::class);
+        }
+
+        return $flexFormService;
+    }
+}
