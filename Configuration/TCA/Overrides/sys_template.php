a2ba6a2562dcd21d7e2b4353fcd62863c2849f40
Sebastian Stein <sebastian.stein@in2code.de>
2022-12-16 11:50:45 +0100

    [TASK] move addStaticFile into sys_template.php
    See: https://docs.typo3.org/m/typo3/reference-coreapi/main/en-us/ExtensionArchitecture/FileStructure/ExtTables.html#should-not-be-used-for

---
+++ Configuration/TCA/Overrides/sys_template.php
@@ -0,0 +1,15 @@
+<?php
+
+declare(strict_types=1);
+
+\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addStaticFile(
+    'in2studyfinder',
+    'Configuration/TypoScript/Main',
+    'In2studyfinder Basic Template'
+);
+
+\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addStaticFile(
+    'in2studyfinder',
+    'Configuration/TypoScript/Css',
+    'In2studyfinder Demo CSS Template'
+);
