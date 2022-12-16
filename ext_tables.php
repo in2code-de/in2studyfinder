4f98a44b9c804dbf2874b33cb425b440987f28ca
Sebastian Stein <sebastian.stein@in2code.de>
2022-12-16 13:24:01 +0100

    [TASK] move the icon registration into Icons.php
    See: https://docs.typo3.org/m/typo3/reference-coreapi/main/en-us/ApiOverview/Icon/Index.html#icon-api

--- ext_tables.php
+++ ext_tables.php
@@ -2,27 +2,17 @@
 
 defined('TYPO3') or die();
 
-/**
- * Register Icons
- */
-$iconRegistry = \TYPO3\CMS\Core\Utility\GeneralUtility::makeInstance(
-    \TYPO3\CMS\Core\Imaging\IconRegistry::class
-);
-$iconRegistry->registerIcon(
-    'in2studyfinder-plugin-icon',
-    \TYPO3\CMS\Core\Imaging\IconProvider\BitmapIconProvider::class,
-    ['source' => 'EXT:in2studyfinder/Resources/Public/Icons/Extension.svg']
-);
+(static function () {
+    /**
+     * Add to ContentElementWizard
+     */
+    \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addPageTSConfig(
+        '<INCLUDE_TYPOSCRIPT: source="FILE:EXT:in2studyfinder/Configuration/TSConfig/ContentElementWizard.typoscript">'
+    );
 
-/**
- * Add to ContentElementWizard
- */
-\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addPageTSConfig(
-    '<INCLUDE_TYPOSCRIPT: source="FILE:EXT:in2studyfinder/Configuration/TSConfig/ContentElementWizard.typoscript">'
-);
-
-/**
- * Register default export types
- */
-$GLOBALS['TYPO3_CONF_VARS']['EXT']['in2studyfinder']['exportTypes']['CSV'] =
-    \In2code\In2studyfinder\Export\ExportTypes\CsvExport::class;
+    /**
+     * Register default export types
+     */
+    $GLOBALS['TYPO3_CONF_VARS']['EXT']['in2studyfinder']['exportTypes']['CSV'] =
+        \In2code\In2studyfinder\Export\ExportTypes\CsvExport::class;
+})();
