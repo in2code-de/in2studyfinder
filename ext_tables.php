d72cda845469bc8ab2ffcc23752fa63f7cdd6e64
Sebastian Stein <sebastian.stein@in2code.de>
2022-12-16 13:25:09 +0100

    [TASK] use automatic inclusion of page TsConfig for custom content element
    See: https://docs.typo3.org/c/typo3/cms-core/main/en-us/Changelog/12.0/Feature-96614-AutomaticInclusionOfPageTsConfigOfExtensions.html?highlight=addpagetsconfig

--- ext_tables.php
+++ ext_tables.php
@@ -4,13 +4,6 @@ defined('TYPO3') or die();
 
 (static function () {
     /**
-     * Add to ContentElementWizard
-     */
-    \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addPageTSConfig(
-        '<INCLUDE_TYPOSCRIPT: source="FILE:EXT:in2studyfinder/Configuration/TSConfig/ContentElementWizard.typoscript">'
-    );
-
-    /**
      * Register default export types
      */
     $GLOBALS['TYPO3_CONF_VARS']['EXT']['in2studyfinder']['exportTypes']['CSV'] =
