37b7515fa8dcae977faa1c40c8518970c44c55e1
Bastien Lutz <bastien.lutz@in2code.de>
2025-06-23 15:11:39 +0200

    [TASK] Add chatbot to content element wizard

--- ext_tables.php
+++ ext_tables.php
@@ -69,6 +69,11 @@ if (TYPO3_MODE === 'BE') {
         \TYPO3\CMS\Core\Imaging\IconProvider\BitmapIconProvider::class,
         ['source' => 'EXT:in2studyfinder/Resources/Public/Icons/Extension.svg']
     );
+    $iconRegistry->registerIcon(
+        'in2studyfinder-chatbot-icon',
+        \TYPO3\CMS\Core\Imaging\IconProvider\SvgIconProvider::class,
+        ['source' => 'EXT:in2studyfinder/Resources/Public/Icons/chatbot.svg']
+    );
 
     /**
      * Add to ContentElementWizard
