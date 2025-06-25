1715b4233b7b015da776f0f1f1fbff70a80681c8
Franz Kugelmann <franz.kugelmann@elementare-teilchen.de>
2025-06-25 13:05:16 +0200

    [FEATURE] add sorting possibility to filter items

--- Configuration/TCA/tx_in2studyfinder_domain_model_graduation.php
+++ Configuration/TCA/tx_in2studyfinder_domain_model_graduation.php
@@ -20,6 +20,7 @@ return [
         'transOrigPointerField' => 'l10n_parent',
         'transOrigDiffSourceField' => 'l10n_diffsource',
         'delete' => 'deleted',
+        'sortby' => 'sorting',
         'enablecolumns' => [
             'disabled' => 'hidden',
             'starttime' => 'starttime',
