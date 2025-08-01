2aeb9755db2410e5d12e40c05f728bfb45903bef
Sebastian Stein <sebastian.stein@in2code.de>
2025-08-01 15:57:11 +0200

    [CLEANUP] remove obsolete functions

--- Classes/Domain/Service/CourseService.php
+++ Classes/Domain/Service/CourseService.php
@@ -11,9 +11,6 @@ use In2code\In2studyfinder\Service\PluginService;
 use In2code\In2studyfinder\Utility\CacheUtility;
 use TYPO3\CMS\Core\MetaTag\MetaTagManagerRegistry;
 use TYPO3\CMS\Core\Utility\GeneralUtility;
-use TYPO3\CMS\Extbase\Reflection\ClassSchema\Property;
-use TYPO3\CMS\Extbase\Reflection\Exception\UnknownClassException;
-use TYPO3\CMS\Extbase\Reflection\ReflectionService;
 
 /**
  * @SuppressWarnings(PHPMD.LongVariable)
@@ -22,17 +19,10 @@ class CourseService extends AbstractService
 {
     protected array $settings = [];
 
-    /**
-     * @var ReflectionService|null
-     */
-    protected ?ReflectionService $reflectionService = null;
-
     public function __construct(
         protected StudyCourseRepository $studyCourseRepository,
         protected PluginService $pluginService
-    ) {
-        $this->reflectionService = GeneralUtility::makeInstance(ReflectionService::class);
-    }
+    ) {}
 
     public function findBySearchOptions(array $searchOptions, array $pluginRecord): array
     {
@@ -71,59 +61,5 @@ class CourseService extends AbstractService
                 $studyCourse->getMetaKeywords()
             );
         }
-    }
-
-    public function getCourseProperties(StudyCourse $course, array $excludedFields = []): array
-    {
-        return $this->getCoursePropertyList(
-            $this->reflectionService->getClassSchema($course)->getProperties(),
-            $excludedFields
-        );
-    }
-
-    /**
-     * @param Property[] $objectProperties
-     * @throws UnknownClassException
-     */
-    private function getCoursePropertyList(
-        array $objectProperties,
-        array $excludedFields
-    ): array {
-        $propertyArray = [];
-
-        foreach ($objectProperties as $property) {
-            if (
-                !in_array(
-                    $property->getName(),
-                    $excludedFields,
-                    true
-                )
-            ) {
-                $propertyName = $property->getName();
-                $type = $property->getPrimaryType()->getBuiltinType();
-                if ($type !== 'object') {
-                    $propertyArray[$propertyName] = $propertyName;
-                    continue;
-                }
-
-                $className = $property->getPrimaryType()->getClassName();
-                if ($property->isObjectStorageType()) {
-                    $childPropertyClassName = $property->getPrimaryCollectionValueType()->getClassName();
-                    if (class_exists($childPropertyClassName)) {
-                        $propertyArray[$propertyName] = $this->getCoursePropertyList(
-                            $this->reflectionService->getClassSchema($childPropertyClassName)->getProperties(),
-                            $excludedFields
-                        );
-                    }
-                } else {
-                    $propertyArray[$propertyName] = $this->getCoursePropertyList(
-                        $this->reflectionService->getClassSchema($className)->getProperties(),
-                        $excludedFields
-                    );
-                }
-            }
-        }
-
-        return $propertyArray;
     }
 }
